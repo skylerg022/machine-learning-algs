0cf1b77d991a605b1a9ef4da250343c11f4e83f7
skylerg022 <skylerg022@gmail.com>
2020-05-05 17:08:42 -0600

    Added cluster algs and cleaned repo

--- clustering/HAC.py
+++ clustering/HAC.py
@@ -1,37 +1,93 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClusterMixin
 
-class HACClustering(BaseEstimator,ClusterMixin):
+class HACClustering(BaseEstimator, ClusterMixin):
 
-    def __init__(self,k=3,link_type='single'): ## add parameters here
+    def __init__(self, k=3, link_type='single'):
         """
         Args:
-            k = how many final clusters to have
-            link_type = single or complete. when combining two clusters use complete link or single link
+            k (int): the number of clusters at which to stop the cluster-grouping algorithm
+            link_type (str): either 'single' or 'complete'; single-link determines the closeness of two clusters
+                using the distance of the closest two points, both in a different cluster; complete-link uses the 
+                distance of the furthest two points between two clusters
         """
+        
         self.link_type = link_type
         self.k = k
-    def fit(self,X,y=None):
-        """ Fit the data; In this lab this will make the K clusters :D
+        
+    def fit(self, X):
+        """
         Args:
-            X (array-like): A 2D numpy array with the training data
-            y (array-like): An optional argument. Clustering is usually unsupervised so you don't need labels
+            X: A 2D numpy array with the training data
         Returns:
-            self: this allows this to be chained, e.g. model.fit(X,y).predict(X_test)
+            self: this allows function chaining, e.g. model.fit(X).save_clusters('clusters.txt')
         """
+        
+        self.out = []
+        clusters = [[i] for i in range(X.shape[0])]
+        while len(clusters) > self.k: # Combine clusters while there are more than k clusters
+            best_clusters = ()
+            best_dist = np.inf
+            
+            # Determine closest two clusters
+            for c1 in range(len(clusters)-1):
+                for c2 in range(c1+1, len(clusters)):
+                    furthest_dist = -1
+                    cluster1 = clusters[c1]
+                    cluster2 = clusters[c2]
+                    for i in cluster1:
+                        for j in cluster2:
+                            dist = np.round(np.sum( (X[i]-X[j])**2 )**0.5, 6)
+                            if dist < best_dist and self.link_type == 'single':
+                                best_dist = dist
+                                best_clusters = (c1, c2)
+                            elif dist > furthest_dist and self.link_type == 'complete':
+                                furthest_dist = dist
+                    if furthest_dist < best_dist and self.link_type == 'complete':
+                        best_dist = furthest_dist
+                        best_clusters = (c1, c2)
+            
+            c1, c2 = best_clusters
+            clusters[c1] = np.concatenate((clusters[c1], clusters[c2]))
+            clusters = np.delete(clusters, c2)
+        
+        # Calculate report variables for save_clusters() function
+        centroids = []
+        clusters_sse = []
+        for c in range(self.k):
+            # Calculate new centroids
+            points = np.array(clusters[c])
+            cent = np.mean(X[points], axis=0)
+            
+            # Calculate SSE for cluster
+            sse = 0
+            for p in points:
+                point = X[p]
+                sse += np.sum( (point-cent)**2 )
+            centroids.append(cent)
+            clusters_sse.append(sse)
+
+        self.centroids = centroids
+        self.clusters = clusters
+        self.clusters_sse = clusters_sse
         return self
-    def save_clusters(self,filename):
+    
+    def save_clusters(self, filename):
         """
-            f = open(filename,"w+") 
-            Used for grading.
-            write("{:d}\n".format(k))
-            write("{:.4f}\n\n".format(total SSE))
-            for each cluster and centroid:
-                write(np.array2string(centroid,precision=4,separator=","))
-                write("\n")
-                write("{:d}\n".format(size of cluster))
-                write("{:.4f}\n\n".format(SSE of cluster))
-            f.close()
+        Args:
+            filename: file path to save summary statistics
+        Returns:
+            Saves algorithm statistics to the path provided
         """
-
-
+        
+        f = open(filename, 'w+')
+        f.write('Clusters: {:d}\n'.format(self.k))
+        f.write('Total SSE: {:.4f}\n\n'.format(sum(self.clusters_sse)))
+        for c in range(self.k):
+            f.write('Centroid {:d} Location: '.format(c))
+            f.write(np.array2string(self.centroids[c],precision=4,separator=',')+'\n')
+            f.write('Observations in Cluster: {:d}\n'.format(len(self.clusters[c])))
+            f.write('Cluster SSE: {:.4f}\n\n'.format(self.clusters_sse[c]))
+        f.close()
+        
+        return
--- clustering/Kmeans.py
+++ clustering/Kmeans.py
@@ -1,36 +1,93 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClusterMixin
+import random
 
-class KMEANSClustering(BaseEstimator,ClusterMixin):
+class KMEANSClustering(BaseEstimator, ClusterMixin):
 
-    def __init__(self,k=3,debug=False): ## add parameters here
+    def __init__(self,k=3,debug=False):
         """
         Args:
-            k = how many final clusters to have
-            debug = if debug is true use the first k instances as the initial centroids otherwise choose random points as the initial centroids.
+            k (int): the number of clusters at which to stop the cluster-grouping algorithm
+            debug (bool): if debug is true, use the first k instances as the initial centroids otherwise choose 
+                random points as the initial centroids.
         """
+        
         self.k = k
         self.debug = debug
 
-    def fit(self,X,y=None):
+    def fit(self, X):
         """ Fit the data; In this lab this will make the K clusters :D
         Args:
-            X (array-like): A 2D numpy array with the training data
-            y (array-like): An optional argument. Clustering is usually unsupervised so you don't need labels
+            X: A 2D numpy array with the training data
         Returns:
-            self: this allows this to be chained, e.g. model.fit(X,y).predict(X_test)
+            self: this allows function chaining, e.g. model.fit(X).save_clusters('clusters.txt')
         """
+        
+        point_indices = range(X.shape[0])
+        if self.debug:
+            centroids = X[:self.k]
+        else:
+            cent = random.sample(point_indices, k=self.k)
+            centroids = X[cent]
+        centroids_moved = True
+        
+        while centroids_moved:
+            centroids_moved = False
+            clusters = [[] for i in range(self.k)]
+            distances = [[] for i in range(self.k)]
+            
+            # Determine which centroid each point is closest to
+            for p in point_indices:
+                point = X[p]
+                best_dist = np.inf
+                best_cent = -1
+                for c in range(self.k):
+                    dist = np.sum( (point-centroids[c])**2 )**.5
+                    if dist < best_dist:
+                        best_dist = dist
+                        best_cent = c
+                clusters[best_cent].append(p) # Add point into closest cluster
+                distances[best_cent].append(best_dist)# Add calculated distance into closest cluster
+            
+            new_centroids = []
+            clusters_sse = []
+            for c in range(self.k):
+                # Calculate cluster SSE
+                sse = np.sum( np.array(distances[c])**2 )
+                clusters_sse.append(sse)
+                
+                # Calculate new centroids
+                points = clusters[c]
+                new_cent = np.mean(X[points], axis=0)
+                new_centroids.append(new_cent)
+            
+            new_centroids = np.array(new_centroids)
+            if (new_centroids != centroids).any():
+                centroids_moved = True
+            centroids = new_centroids
+        
+        # Save final iteration variables for reporting
+        self.report = (centroids, clusters, clusters_sse)
         return self
+    
     def save_clusters(self,filename):
         """
-            f = open(filename,"w+") 
-            Used for grading.
-            write("{:d}\n".format(k))
-            write("{:.4f}\n\n".format(total SSE))
-            for each cluster and centroid:
-                write(np.array2string(centroid,precision=4,separator=","))
-                write("\n")
-                write("{:d}\n".format(size of cluster))
-                write("{:.4f}\n\n".format(SSE of cluster))
-            f.close()
+        Args:
+            filename: file path to save summary statistics
+        Returns:
+            Saves algorithm statistics to the path provided
         """
+        
+        centroids, clusters, clusters_sse = self.report
+        
+        f = open(filename, 'w+')
+        f.write('Clusters: {:d}\n'.format(self.k))
+        f.write('Total SSE: {:.4f}\n\n'.format(sum(clusters_sse)))
+        for c in range(self.k):
+            f.write('Centroid {:d} Location: '.format(c))
+            f.write(np.array2string(centroids[c],precision=4,separator=',')+'\n')
+            f.write('Observations in Cluster: {:d}\n'.format(len(clusters[c])))
+            f.write('Cluster SSE: {:.4f}\n\n'.format(clusters_sse[c]))
+        f.close()
+        
+        return
